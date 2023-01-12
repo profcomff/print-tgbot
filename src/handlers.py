2d2e153c87312a15370fe4750a88655f3fab57a7
Andrey Marakulin <annndruha.github@gmail.com>
2023-01-12 17:12:34 +0500

    Refactor

--- src/handlers.py
+++ src/handlers.py
@@ -3,8 +3,6 @@
 
 import os
 import time
-import json
-import html
 import logging
 import requests
 import traceback
@@ -24,26 +22,27 @@ def handler(func):
         try:
             await func(update, context)
         except psycopg2.Error as err:
-            logging.error('Database Error (longpull_loop), description:')
+            logging.error('Database Error (wrapper), description:')
             traceback.print_tb(err.__traceback__)
             logging.error(err.args)
             try:
-                logging.warning('Try to recconnect database...')
+                logging.warning('Try to reconnect database...')
                 db.reconnect()
                 logging.warning('Database connected successful')
                 time.sleep(1)
             except psycopg2.Error:
-                logging.error('Recconnect database failed')
+                logging.error('Reconnect database failed')
                 time.sleep(10)
 
         except Exception as err:
             await context.bot.send_message(
                 chat_id=update.effective_user.id, text=ans['im_broken'],
                 parse_mode=telegram.constants.ParseMode('HTML'))
-            logging.error('BaseException (longpull_loop), description:')
+            logging.error('Exception (wrapper), description:')
             traceback.print_tb(err.__traceback__)
             logging.error(str(err.args))
             time.sleep(5)
+
     return wrapper
 
 
@@ -57,7 +56,7 @@ async def error_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> N
 async def handler_start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [[InlineKeyboardButton(ans['conf'], callback_data='want_confident')]]
     text = ans['help']
-    if __auth(update, context) is None:
+    if __auth(update) is None:
         text += ans['val_addition']
         keyboard.append([InlineKeyboardButton(ans['auth'], callback_data='auth')])
     await update.message.reply_text(text=text,
@@ -70,7 +69,7 @@ async def handler_button(update: Update, context: CallbackContext) -> None:
     if update.callback_query.data == 'want_instruction':
         text = ans['help']
         keyboard = [[InlineKeyboardButton(ans['conf'], callback_data='want_confident')]]
-        if __auth(update, context) is None:
+        if __auth(update) is None:
             keyboard.append([InlineKeyboardButton(ans['auth'], callback_data='auth')])
             text += ans['val_addition']
         reply_markup = InlineKeyboardMarkup(keyboard)
@@ -78,7 +77,7 @@ async def handler_button(update: Update, context: CallbackContext) -> None:
     elif update.callback_query.data == 'want_confident':
         text = ans['conf_full']
         keyboard = [[InlineKeyboardButton(ans['back'], callback_data='want_instruction')]]
-        if __auth(update, context) is None:
+        if __auth(update) is None:
             keyboard.append([InlineKeyboardButton(ans['auth'], callback_data='auth')])
             text += ans['val_addition']
         reply_markup = InlineKeyboardMarkup(keyboard)
@@ -87,7 +86,7 @@ async def handler_button(update: Update, context: CallbackContext) -> None:
         text = ans['val_need']
         reply_markup = None
     elif update.callback_query.data.startswith('print_'):
-        await __print_settings_solver(update, context)
+        await __print_settings_solver(update)
         return
     else:
         text = ans['unknown_query']
@@ -107,7 +106,7 @@ async def handler_help(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 @handler
 async def handler_auth(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    requisites = __auth(update, context)
+    requisites = __auth(update)
     if requisites is None:
         await update.message.reply_text(ans['val_need'])
     else:
@@ -131,20 +130,16 @@ async def handler_mismatch_doctype(update: Update, context: ContextTypes.DEFAULT
     await update.message.reply_text(ans['only_pdf'])
 
 
-async def __print_settings_solver(update, context):
-    a = update.message
-    filename = update.callback_query.message.reply_to_message.document.file_name
+async def __print_settings_solver(update):
+    # filename = update.callback_query.message.reply_to_message.document.file_name
     _, button, pin = update.callback_query.data.split('_')
 
     r = requests.get(config.PRINT_URL + f'''/file/{pin}''')
     if r.status_code == 200:
         options = r.json()['options']
-        # print(r.elapsed.total_seconds())
     else:
         await update.callback_query.message.reply_text('Сервер не ответил, попробуйте позже.')
         return
-
-    print('FROM:', button, 'Двухсторонняя печать' if options['two_sided'] else 'Односторонняя печать')
 
     if button == 'copies':
         options['copies'] += 1
@@ -152,23 +147,19 @@ async def __print_settings_solver(update, context):
         options['two_sided'] = not options['two_sided']
 
     r = requests.patch(config.PRINT_URL + f'''/file/{pin}''', json={'options': options})
-    # print(r.elapsed.total_seconds())
     if r.status_code != 200:
         await update.callback_query.message.reply_text('Сервер не ответил, настройки не изменены, попробуйте позже.')
         return
 
-    sidetest = 'Двухсторонняя печать' if options['two_sided'] else 'Односторонняя печать'
-    print('TO:', button, sidetest)
-
-
     keyboard = [[InlineKeyboardButton(f'Копий: {options["copies"]}', callback_data=f'print_copies_{pin}')],
-                [InlineKeyboardButton(sidetest, callback_data=f'print_twosided_{pin}')]]
+                [InlineKeyboardButton('Двухсторонняя печать' if options['two_sided'] else 'Односторонняя печать',
+                                      callback_data=f'print_twosided_{pin}')]]
     reply_markup = InlineKeyboardMarkup(keyboard)
     await update.callback_query.edit_message_reply_markup(reply_markup=reply_markup)
 
 
 async def handler_print(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    requisites = __auth(update, context)
+    requisites = __auth(update)
     if requisites is None:
         await context.bot.send_message(chat_id=update.message.chat.id,
                                        text=ans['doc_not_accepted'])
@@ -259,7 +250,7 @@ async def __get_attachments(update, context):
     return path_to_save, update.message.document.file_name  # , update.message.document.file_unique_id
 
 
-def __auth(update, context):
+def __auth(update):
     chat_id = update.effective_user.id
     if db.get_user(chat_id) is not None:
         tg_id, surname, number = db.get_user(chat_id)
