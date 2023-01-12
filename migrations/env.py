8d3adcade33e76044931cd5f849950b27ce901d9
Andrey Marakulin <annndruha.github@gmail.com>
2023-01-12 22:46:34 +0500

    Test

--- migrations/env.py
+++ migrations/env.py
@@ -4,13 +4,14 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-from ..src.db import Base
-from ..src.settings import get_settings
+from .. import src
+# from ..src.db import Base
+# from ..src.settings import get_settings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-settings = get_settings()
+settings = src.settings.get_settings()
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -21,7 +22,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = Base.metadata
+target_metadata = src.db.Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
