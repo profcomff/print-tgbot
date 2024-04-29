6d3df9479de9a8a238c88c8beef702a7d9b557b7
iskomir <cop1977_77@mail.ru>
2024-04-29 16:11:07 +0300

    Update handlers.py

--- src/handlers.py
+++ src/handlers.py
@@ -153,17 +153,17 @@ async def handler_print(update: Update, context: ContextTypes.DEFAULT_TYPE):
             return
         elif r.status_code == 413:
             await update.message.reply_text(
-            text=ans.file_size_error.format(update.message.document.file_name),
-            reply_to_message_id=update.message.id,
-            parse_mode=ParseMode('HTML'),
+                text=ans.file_size_error.format(update.message.document.file_name),
+                reply_to_message_id=update.message.id,
+                parse_mode=ParseMode('HTML'),
             )
             logging.warning(f'{log_actor(update)} print api 413 SizeErr')
             return
         elif r.status_code == 415:
             await update.message.reply_text(
-            text=ans.unreadable_file_error.format(update.message.document.file_name),
-            reply_to_message_id=update.message.id,
-            parse_mode=ParseMode('HTML'),
+                text=ans.unreadable_file_error.format(update.message.document.file_name),
+                reply_to_message_id=update.message.id,
+                parse_mode=ParseMode('HTML'),
             )
             logging.warning(f'{log_actor(update)} print api 415 UnreadableErr')
             return
